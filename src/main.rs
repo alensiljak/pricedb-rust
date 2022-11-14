a39bc71d64ed2669aabd3aeaad06cccbea0d5b92
Alen Šiljak <dev@alensiljak.eu.org>
2022-11-14 13:04:24 +0100

    cli definition, dl command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    name: Option<String>,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    // #[arg(short, long)]
+    // argument: String,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    dl {}
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let cli = Cli::parse();
+
+    println!("Hello {:?}!", cli.command)
 }
