09a11d6aef16cb8939d86e23cdb76d5cad0efa88
Alen Šiljak <dev@alensiljak.eu.org>
2022-11-14 13:38:35 +0100

    trying match

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,32 @@ struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
+    // Add
+    // Cfg
     Dl {
+        // exchange, -x
+        // symbol
+        // agent
         #[arg(short, long)]
         currency: Option<String>,
     },
+    // export
+    // import
+    // last
+    // list
+    Prune {
+        //all: Option<>,
+        symbol: Option<String>
+    },
+    // securities
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    println!("Hello {:?}!", cli.command)
+    println!("Hello {:?}!", cli.command);
+    match cli.command {
+        Some(dl) => print!("yo"),
+        _ => print!("No command issued"),
+    }
 }
