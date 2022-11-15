684ecdcf4c7455ffc7bc9178799767c30eec1cc9
Alen Šiljak <dev@alensiljak.eu.org>
2022-11-15 12:40:04 +0100

    specifying the argument details

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
+#[command(name="Price Database")]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     #[command(subcommand)]
@@ -12,16 +13,20 @@ enum Commands {
     // Add
     // Cfg
     Dl {
-        // exchange, -x
-        // symbol
-        // agent
+        //#[arg(short='x', long)]
+        // todo: exchange: Option<String>,
+
+        // todo: symbol
+        // todo: agent
+
         #[arg(short, long)]
         currency: Option<String>,
     },
-    // export
-    // import
-    // last
-    // list
+
+    // todo: export
+    // todo: import
+    // todo: last
+    // todo: list
     Prune {
         //all: Option<>,
         symbol: Option<String>
@@ -32,16 +37,21 @@ enum Commands {
 fn main() {
     let cli = Cli::parse();
 
-    println!("Hello {:?}!", cli.command);
+    println!("Debug: {:?}!", cli.command);
 
-    match cli.command {
-        Some(Commands::Dl { currency }) => {
-            if currency.is_some() {
-                println!("download for currency {:?}", currency)
-            } else {
-                println!("plain download")
-            }
-        },
+    match &cli.command {
+        Some(Commands::Dl { currency: _ }) => download(cli.command),
         _ => println!("No command issued"),
     }
+}
+
+fn download(dl: Option<Commands>) {
+    println!("{:?}", dl);
+    //dl = Commands::Dl
+    // if currency.is_some() {
+    //     println!("download for currency {:?}", currency)
+    // } else {
+    //     println!("plain download")
+    // }
+
 }
