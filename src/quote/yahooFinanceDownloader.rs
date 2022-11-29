c94fcb93b36a8cc601eb2319e93b5b30232a361e
Alen Šiljak <dev@alensiljak.eu.org>
2022-11-29 23:04:09 +0100

    slow grind

--- src/quote/yahooFinanceDownloader.rs
+++ src/quote/yahooFinanceDownloader.rs
@@ -1,4 +1,6 @@
-use crate::model::Price;
+use std::collections::HashMap;
+
+use crate::model::{Price, SecuritySymbol};
 
 use super::Downloader;
 
@@ -6,17 +8,34 @@ use super::Downloader;
 #[derive(Debug)]
 pub struct YahooFinanceDownloader {
     url: String,
+    namespaces: HashMap<&'static str, &'static str>
 }
 
 impl YahooFinanceDownloader {
     pub fn new() -> YahooFinanceDownloader {
+        let namespaces = HashMap::from([
+            ("AMS", "AS"),
+            ("ASX", "AX"),
+            ("BATS", ""),
+            ("BVME", "MI"),
+            ("FWB", "F"),
+            ("LSE", "L"),
+            ("NASDAQ", ""),
+            ("NYSE", ""),
+            ("NYSEARCA", ""),
+            ("XETRA", "DE")
+        ]);
+
         YahooFinanceDownloader {
             url: "https://query1.finance.yahoo.com/v8/finance/chart/".to_string(),
+            namespaces
         }
     }
 
-    fn assemble_url(&self) -> &String {
-        
+    fn assemble_url(&self, symbol: SecuritySymbol) -> &String {
+        if self.namespaces.keys().any(|key| key == &symbol.namespace.as_str()) {
+            log::debug!("found!");
+        }
         todo!("assemble the url");
 
         return &self.url;
@@ -30,7 +49,7 @@ impl Downloader for YahooFinanceDownloader {
         security_symbol: crate::model::SecuritySymbol,
         currency: &str,
     ) -> crate::model::Price {
-        let url = self.assemble_url();
+        let url = self.assemble_url(security_symbol);
 
         todo!("download price here");
 
