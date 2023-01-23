529df4b45550e266674cde909992c3188ac1d8a5
Alen Šiljak <dev@alensiljak.eu.org>
2023-01-23 13:57:53 +0100

    enabling formatting tests

--- src/ledger_formatter.rs
+++ src/ledger_formatter.rs
@@ -26,15 +26,6 @@ pub(crate) fn format_prices(prices: Vec<Price>, securities: Vec<SymbolMetadata>)
     output
 }
 
-// pub fn format_prices_w_symbols(prices: Vec<Price>) -> String {
-//     let mut output = String::default();
-//     for pws in prices {
-//         output += format_price_w_symbol(&pws).as_str();
-//         output += "\n";
-//     }
-//     output
-// }
-
 /** Formats a single Price record.
  * ledger price format, ISO format supported:
  * P 2004-06-21 02:17:58 VTI $27.76
@@ -55,58 +46,47 @@ fn format_price(price: &Price, sec: &SymbolMetadata) -> String {
     format!("P {date_time} {symbol} {value} {currency}")
 }
 
-// fn format_price_w_symbol(pws: &Price) -> String {
-//     let date = pws.date.to_owned();
-//     let time = pws.time.to_owned();
-//     let date_time = format!("{date} {time}");
-
-//     let symbol = if pws.ledger_symbol.is_empty() {
-//         &pws.symbol
-//     } else {
-//         &pws.ledger_symbol
-//     };
-//     let value = pws.value;
-//     let currency = &pws.currency;
-
-//     format!("P {date_time} {symbol} {value} {currency}")
-// }
-
 #[cfg(test)]
 mod tests {
 
     // use super::*;
 
-    /*
-       #[test]
-       fn test_single_price_formatting() {
-           let mut symbol = Security::new();
-           symbol.ledger_symbol = Some("EL4X_DE".into());
+    use as_symbols::SymbolMetadata;
 
-           let price = Price {
-               id: 113,
-               security_id: 26,
-               date: "2022-12-01".into(),
-               time: "12:25:34".into(),
-               value: 12534,
-               denom: 100,
-               currency: "EUR".into(),
-           };
+    use crate::{ledger_formatter::format_price, model::Price};
 
-           let actual = format_price(&price, &symbol);
-
-           // println!("{actual:?}");
-
-           assert_eq!(actual, "P 2022-12-01 12:25:34 EL4X_DE 125.34 EUR");
-       }
-    */
-
-    /*
     #[test]
-    fn test_format_price_wo_time() {
-        let mut symbol = Security::new();
-        symbol.ledger_symbol = Some("VAS_AX".into());
+    fn test_single_price_formatting() {
+        let mut sm = SymbolMetadata::new();
+        sm.symbol = "EL4X".into();
+        sm.ledger_symbol = Some("EL4X_DE".into());
 
         let price = Price {
+            symbol: "EL4X".to_string(),
+            id: 113,
+            security_id: 26,
+            date: "2022-12-01".into(),
+            time: "12:25:34".into(),
+            value: 12534,
+            denom: 100,
+            currency: "EUR".into(),
+        };
+
+        let actual = format_price(&price, &sm);
+
+        // println!("{actual:?}");
+
+        assert_eq!(actual, "P 2022-12-01 12:25:34 EL4X_DE 125.34 EUR");
+    }
+
+    #[test]
+    fn test_format_price_wo_time() {
+        let mut sm = SymbolMetadata::new();
+        sm.symbol = "VAS".into();
+        sm.ledger_symbol = Some("VAS_AX".into());
+
+        let price = Price {
+            symbol: "VAS".into(),
             id: 113,
             security_id: 26,
             date: "2022-12-01".into(),
@@ -116,11 +96,10 @@ mod tests {
             currency: "AUD".into(),
         };
 
-        let actual = format_price(&price, &symbol);
+        let actual = format_price(&price, &sm);
 
         // println!("{actual:?}");
 
         assert_eq!(actual, "P 2022-12-01 00:00:00 VAS_AX 125.34 AUD");
     }
-     */
 }
